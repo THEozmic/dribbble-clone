907c906ab08cb05a54e245732c22be5ed93135bb
Michael Ozoemena <michaelozoemena2@gmail.com>
2017-11-15 19:59:52 +0100

    [FT] Add modal UI and toggle functionality

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -4,6 +4,29 @@ import '../css/style.scss';
 import shots from './shots';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.state = {
+      isModalVisible: false,
+      shot: {}
+    };
+  }
+
+  onClick (event, isToClose, shot = {}) {
+    event.preventDefault();
+    if (isToClose) {
+      this.setState({
+        isModalVisible: false
+      });
+    } else {
+      this.setState({
+        isModalVisible: true,
+        shot
+      });
+    }
+  }
+
   render () {
     return (
       <div>
@@ -19,7 +42,11 @@ class App extends React.Component {
                           <div className="content">
                             <div className="card-image">
                               <div className="is-marginless image p-b-0 p-10">
-                                <div className="overlay">
+                                <a
+                                  className="overlay"
+                                  href="#/shot/8537493"
+                                  onClick={(event) => this.onClick(event, false, shot)}
+                                >
                                   <div className="overlay-text p-20">
                                     <h4><strong>Birds / Stamps collection</strong></h4>
                                     <p>
@@ -31,7 +58,7 @@ class App extends React.Component {
                                       November 14, 2017
                                     </div>
                                   </div>
-                                </div>
+                                </a>
                                 <img
                                   className="card-img"
                                   src="https://cdn.dribbble.com/users/1055435/screenshots/3940714/walk-cycle-012_still_2x.gif"
@@ -84,6 +111,58 @@ class App extends React.Component {
             )
           }
         </div>
+        { this.state.isModalVisible ? 
+          <div className="shot-modal">
+            <div className="modal is-active">
+              <div
+                className="modal-background"
+                onClick={(event) => this.onClick(event, true)}
+              ></div>
+              <div className="modal-content">
+                <div className="box shot-box">
+                  <div className="section">
+                    <div>
+                      <img
+                        className="image is-24x24 avartar large"
+                        src="https://cdn.dribbble.com/users/1055435/avatars/normal/69dec8ee6bc9fb84ab3105ed199f204a.jpg?1480127236"/>
+                      <span className="shot-modal-user m-l-10">
+                        <div>
+                          <strong>Birds / Stamps collection</strong>
+                        </div>
+                        <div className="shot-modal-date">
+                          by
+                          <span className="highlight m-5">
+                            {this.state.shot.name}
+                          </span>
+                          on Nov 14, 2017
+                        </div>
+                      </span>
+                      <div className="card box shot-box is-paddingless m-t-20">
+                        <div className="content">
+                          <div className="card-image">
+                            <div className="is-marginless image p-10">
+                              <img
+                                className="card-img"
+                                src="https://cdn.dribbble.com/users/1055435/screenshots/3940714/walk-cycle-012_still_2x.gif"
+                                alt="Placeholder image"/>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button
+                className="modal-close is-large"
+                ariaLabel="close"
+                onClick={(event) => this.onClick(event, true)}
+              >
+              </button>
+            </div>
+          </div> :
+          null
+        }
       </div>
     );
   }
